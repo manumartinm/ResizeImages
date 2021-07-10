42ab8235d55c92bbb04bc7bb9bcedaf82a0ac029
Manuel Martin <manuzombiesbf1@gmailc.com>
2021-07-11 00:23:05 +0200

    Resize Images

---
+++ main.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const fs = require('fs')
+const sharp = require('sharp')
+
+const directoryPath = path.join(__dirname, 'images');
+const newImageHeight = 200
+const newImageWidth = 100
+
+fs.readdir(directoryPath, (err, files) => {
+
+    if (err) {
+        return console.log('Ha habido un error: ' + err);
+    } 
+
+    files.forEach((file) => {
+        sharp(`./images/${file}`)
+            .resize({ height: newImageHeight, width: newImageWidth })
+            .toFile(`./resized/resize_${file}`)
+    });
+});
